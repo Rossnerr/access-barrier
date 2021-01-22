e873f9d68e3de2300debc5a5650c36ecea010e58
Vlad <vladbantus02@gmail.com>
2021-01-22 21:39:53 +0200

    changed ALPR secret key

--- src/main/java/com/iot/accessbarrier/controller/AccessBarrierController.java
+++ src/main/java/com/iot/accessbarrier/controller/AccessBarrierController.java
@@ -4,6 +4,7 @@ import com.iot.accessbarrier.dto.RsAccessBarrierDto;
 import com.iot.accessbarrier.service.AccessBarrierService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/access-barrier")
 @RequiredArgsConstructor
@@ -23,7 +26,9 @@ public class AccessBarrierController {
     @ApiOperation(value = "Raises up the barrier if car from the provided image has access to the parking area")
     @PostMapping
     public ResponseEntity<RsAccessBarrierDto> raiseBarrier(@RequestParam(value = "image") MultipartFile image) throws IOException {
+        log.info("New attempt to pass the barrier: " + LocalDateTime.now());
         var response = accessBarrierService.raiseBarrier(image);
+        log.info("Car plate number was recognized: " + response.car.getPlateNumber());
         return ResponseEntity.ok(response);
     }
 }
